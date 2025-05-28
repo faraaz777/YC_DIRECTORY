a54acb3d831e58e10eb91a3f6ec4377ea0b7e6df
Faraaz Ahmed <faraaz611@gmail.com>
2025-05-28 11:38:12 +0530

    form submition and server side actions added

--- auth.ts
+++ auth.ts
@@ -1,44 +1,54 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+
 import { client } from "./sanity/lib/client";
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
-import { writeClient } from "./sanity/lib/write-client";
+import { writeClient } from "@/sanity/lib/write-client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    async signIn({
-      user: { name, email, image },
-      profile: { id, login, bio },
-    }) {
+    async signIn({ user, account, profile }) {
+      // Check if the user exists in Sanity
       const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-        id,
+        id: profile?.id,
       });
+
+      console.log({ existingUser, profile, user });
+
       if (!existingUser) {
+        // User not found in Sanity, create a new user document
         await writeClient.create({
           _type: "author",
-          id,
-          name,
-          username: login,
-          email,
-          image,
-          bio: bio || "",
+          id: profile?.id,
+          name: user?.name,
+          username: profile?.login,
+          email: user?.email,
+          image: user?.image,
+          bio: profile?.bio || "",
         });
       }
+
+      // Return true to continue the sign-in process
+      return true;
     },
     async jwt({ token, account, profile }) {
+      // Add profile id to the JWT token on sign-in
       if (account && profile) {
+        // get the user from sanity
         const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
           id: profile?.id,
         });
-        if(!user){
-          token.id = user._id;
-        }
+
+      
+        token.id = user._id;
       }
       return token;
     },
+
     async session({ session, token }) {
-      Object.assign(session,{id:token.id});
+      // Pass the profile id from the token to the session
+      Object.assign(session, { id: token.id });
       return session;
     },
   },
